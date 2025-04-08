784ae9ab8af517800f3b8e0d4d96ce9ab838b3f9
Naoto Mukai <nmukai@sugiyama-u.ac.jp>
2025-04-08 14:05:50 +0900

    chapter3

---
+++ archives/chapter3/README.md
@@ -0,0 +1,6 @@
+# Project3
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/nmukai/pen/WNogxaj](https://codepen.io/nmukai/pen/WNogxaj).
+
---
+++ archives/chapter3/src/script.js
@@ -0,0 +1,43 @@
+let x; //変数xを宣言
+x = 2; //変数xに整数2を代入
+document.write("<p>x=" + x + "</p>");
+
+let y = 3; //変数yを宣言し，整数3を代入
+document.write("<p>y=" + y + "</p>");
+
+x = 4; //変数xを整数4で上書き
+document.write("<p>x=" + x + "</p>");
+
+const z = 5; //定数zを宣言し，整数5を代入
+document.write("<p>z=" + z + "</p>");
+
+let a = 10;
+let b = "abc";
+let c = true;
+document.write("<p>" + typeof(a) + "</p>");
+document.write("<p>" + typeof(b) + "</p>");
+document.write("<p>" + typeof(c) + "</p>");
+
+let apple = Symbol();
+let fruit = apple;
+document.write("<p>" + typeof(fruit) + "</p>");
+
+let name = "Naoto";
+let hello = `<p>Hello! ${name}</p>`;
+document.write(hello);
+
+document.write(`<p>1+2=${1+2}</p>`);
+document.write(`<p>2-1=${2-1}</p>`);
+document.write(`<p>2*3=${2*3}</p>`);
+document.write(`<p>5/2=${5/2}</p>`);
+document.write(`<p>5%2=${5%2}</p>`);
+document.write(`<p>5**2=${5**2}</p>`);
+
+let price = 100;
+let tax = price * 0.1;
+let total = price + tax;
+document.write(`<p>price=${total}</p>`);
+
+
+
+
